3a5cd2378658a46279093264512fcee8227924be
ewanlaing <117400398+ewanlaing@users.noreply.github.com>
2023-07-31 14:47:51 +0100

    all done

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,34 @@
 public class Printer {
+
+    private int paperLeft;
+    private int tonerLeft;
+
+    public Printer(int paperLeft, int tonerLeft){
+        this.paperLeft = paperLeft;
+        this.tonerLeft = tonerLeft;
+    }
+
+    public int getPaperLeft() {
+        return paperLeft;
+    }
+
+    public void setPaperLeft(int paperLeft) {
+        this.paperLeft = paperLeft;
+    }
+
+    public int getTonerLeft() {
+        return tonerLeft;
+    }
+
+    public void setTonerLeft(int tonerLeft) {
+        this.tonerLeft = tonerLeft;
+    }
+
+    public void print(int pages, int copies){
+        int totalToSubtract = pages * copies;
+        if(getPaperLeft() > totalToSubtract){
+            setPaperLeft(getPaperLeft() - totalToSubtract);
+            setTonerLeft(getTonerLeft() - totalToSubtract);
+        };
+    }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -4,5 +4,25 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PrinterTest {
+    Printer printer;
+
+    @Before
+    public void before(){
+        printer = new Printer (100, 50);
+    }
+
+    @Test
+    public void canPrintIfEnoughPaper(){
+        printer.print(3, 2);
+        assertEquals(94, printer.getPaperLeft());
+        assertEquals(44, printer.getTonerLeft());
+    }
+
+    @Test
+    public void cannotPrintIfNotEnoughPaper(){
+        printer.print(100, 20);
+        assertEquals(100, printer.getPaperLeft());
+        assertEquals(50, printer.getTonerLeft());
+    }
 
 }
